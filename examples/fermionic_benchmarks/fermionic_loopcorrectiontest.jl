76232f03d4441a40d307d15de4bb91757bbd0216
Joey <joeytindall94@googlemail.com>
2026-06-24 10:39:16 -0400

    Stuff

--- examples/fermionic_benchmarks/fermionic_loopcorrectiontest.jl
+++ examples/fermionic_benchmarks/fermionic_loopcorrectiontest.jl
@@ -6,11 +6,11 @@ Random.seed!(1234)
 
 function main()
     ITensors.disable_warn_order()
-    χ = 2
+    χ = 4
     g = named_hexagonal_lattice_graph(4,4)
 
     println("Random hexagonal fermionic tensor network state of BD $(χ)")
-    s = siteinds("fermion", g)
+    s = siteinds("spinful_fermion", g)
     ψf = random_fermionic_tensornetworkstate(g, s; bond_dimension = χ)
     ψf_bpc = BeliefPropagationCache(ψf)
     ψf_bpc = update(ψf_bpc)
@@ -18,15 +18,22 @@ function main()
 
     ψf = network(ψf_bpc)
 
-    norm_bp = norm_sqr(ψf; alg = "bp")
-    norm_lc = norm_sqr(ψf; alg = "loopcorrections", max_configuration_size = 6)
-    norm_bmps = norm_sqr(ψf; alg = "boundarymps", mps_bond_dimension = 32)
-    norm_exact= norm_sqr(ψf; alg = "exact")
+    v = first(center(g))
+    ndn_bp = expect(ψf, (["NupNdn"], [v]); alg = "bp")
+    #ndn_exact = expect(ψf, (["NupNdn"], [v]); alg = "exact")
+    t1 = time()
+    ndn_lc = expect(ψf, (["NupNdn"], [v]); alg = "loopcorrections", max_configuration_size =6, ε = 1e-3)
+    t2 = time()
+    ndn_lc_ad = expect(ψf, (["NupNdn"], [v]); alg = "loopcorrections", max_configuration_size =6, autodiff = true)
+    t3 = time()
 
-    println("BP norm is $norm_bp")
-    println("First order LC norm is $norm_lc")
-    println("BoundaryMPS norm with MPS dim 32 is $norm_bmps")
-    println("Exact norm is $norm_exact")
+    #@show ndn_exact
+    @show ndn_bp
+    @show ndn_lc
+    @show ndn_lc_ad
+
+    println("Non AD took $(t2 - t1) secs")
+    println("AD took $(t3 - t2) secs")
 end
 
 
