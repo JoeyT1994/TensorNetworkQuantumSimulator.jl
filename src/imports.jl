00e81492841e0f40c84a3f7a16eece6e5bcff0c5
lkdvos <ldevos98@gmail.com>
2026-07-08 17:21:50 -0400

    use OMEICO ExhaustiveSearch

--- src/imports.jl
+++ src/imports.jl
@@ -44,8 +44,6 @@ using NamedGraphs.GraphsExtensions:
 
 using NamedGraphs.NamedGraphGenerators: named_grid, named_hexagonal_lattice_graph, named_comb_tree, named_path_graph
 
-using TensorOperations
-
 using ITensors: ITensors
 using ITensors: Index, ITensor, hasqns, noncommonind, combinedind, combiner, replaceinds, sim, onehot, delta, plev, dense, unioninds, uniqueinds, commonind, commoninds, replaceind, datatype, inds, dag, noprime, factorize_svd, prime, hascommoninds, itensor, map_diag!, @Algorithm_str, scalar, @OpName_str, @SiteType_str, denseblocks, tags, op, apply, contract, inner
 
